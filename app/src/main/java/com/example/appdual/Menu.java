4c2916de285770b0e9fe173253d083d9cfd38328
cf19adan.montesinos <cf19adan.montesinos>
2021-02-10 17:22:36 +0100

    Adam

--- app/src/main/java/com/example/appdual/Menu.java
+++ app/src/main/java/com/example/appdual/Menu.java
@@ -2,7 +2,9 @@ package com.example.appdual;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void BuscarPeli(View view){
+        Intent a = new Intent (this, BuscarPeli.class);
+        startActivity(BuscarPeli);
+    }
+
+    public void Social(View view){
+        Intent b = new Intent (this, Social.class);
+        startActivity(Social);
+    }
+
+    public void Perfil(View view){
+        Intent c = new Intent (this, Perfil.class);
+        startActivity(Perfil);
     }
 }
