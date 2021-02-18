16eacceba20f04d26e66b8696cbbc1a327d782c5
cf19adan.montesinos <cf19adan.montesinos>
2021-02-18 16:20:37 +0100

    Adam

--- app/src/main/java/com/example/appdual/BuscarPeli.java
+++ app/src/main/java/com/example/appdual/BuscarPeli.java
@@ -2,6 +2,8 @@ package com.example.appdual;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class BuscarPeli extends AppCompatActivity {
@@ -11,4 +13,18 @@ public class BuscarPeli extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscar_peli);
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search);
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
+    }
+
 }
