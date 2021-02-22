0c2a1c72f060ecbafdac8801eb747e1b11a482ac
cf19adan.montesinos <cf19adan.montesinos>
2021-02-22 17:10:09 +0100

    Adam

--- app/src/main/java/com/example/appdual/BuscarPeli.java
+++ app/src/main/java/com/example/appdual/BuscarPeli.java
@@ -2,9 +2,12 @@ package com.example.appdual;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.SearchManager;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class BuscarPeli extends AppCompatActivity {
 
@@ -12,6 +15,30 @@ public class BuscarPeli extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscar_peli);
+    }
+
+    public void showTxtSearch(){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Busca una pelicula");
+
+        final EditText input = new EditText(this);
+        input.setHint("Film");
+        alert.setView(input);
+
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String pokSearch = input.getText().toString();
+                fetchData process = new fetchData(pokSearch);
+                process.execute();
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Canceled.
+            }
+        });
+        alert.show();
     }
 
 }
