fa8cde7fde7cd2456ca15d7fb7d8ac18444aee97
cf19adan.montesinos <cf19adan.montesinos>
2021-02-22 16:57:50 +0100

    Adam

--- app/src/main/java/com/example/appdual/BuscarPeli.java
+++ app/src/main/java/com/example/appdual/BuscarPeli.java
@@ -14,17 +14,4 @@ public class BuscarPeli extends AppCompatActivity {
         setContentView(R.layout.activity_buscar_peli);
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.search);
-
-        // Get the intent, verify the action and get the query
-        Intent intent = getIntent();
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            doMySearch(query);
-        }
-    }
-
 }
