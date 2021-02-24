4ddf9098a7c877fe2c0e2d91c682a905d4511fcb
cf19adan.montesinos <cf19adan.montesinos>
2021-02-24 17:29:43 +0100

    Adam

--- app/src/main/java/com/example/appdual/BuscarPeli.java
+++ app/src/main/java/com/example/appdual/BuscarPeli.java
@@ -7,6 +7,8 @@ import android.app.SearchManager;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class BuscarPeli extends AppCompatActivity {
@@ -17,23 +19,30 @@ public class BuscarPeli extends AppCompatActivity {
         setContentView(R.layout.activity_buscar_peli);
     }
 
+    Button btnSearch = findViewById(R.id.BPelis2);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+
+        }
+    });
+
     public void showTxtSearch(){
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        alert.setTitle("Busca una pelicula");
+        alert.setTitle("Busca Pelicula");
 
         final EditText input = new EditText(this);
         input.setHint("Film");
         alert.setView(input);
 
-        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+        alert.setPositiveButton("Confirma", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
-                String pokSearch = input.getText().toString();
-                fetchData process = new fetchData(pokSearch);
+                String movie = input.getText().toString();
+                fetchData process = new fetchData(movie);
                 process.execute();
             }
         });
 
-        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        alert.setNegativeButton("Cancela", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
                 // Canceled.
             }
