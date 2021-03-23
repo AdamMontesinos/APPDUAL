89a3cb4645fb69642e94a4ef56e355ff22f104d4
cf19adan.montesinos <cf19adan.montesinos>
2021-03-23 16:07:36 +0100

    Adam

---
+++ app/src/main/java/com/example/appdual/Class/Film.java
@@ -0,0 +1,19 @@
+package com.example.appdual.Class;
+
+public class Film {
+    public String nombrepeli;
+    public String status;
+
+    public Film(String title) {
+        this.nombrepeli = nombrepeli;
+    }
+
+    public String getNombrepeli() {
+        return nombrepeli;
+    }
+
+    public void setNombrepeli(String nombrepeli) {
+        this.nombrepeli = nombrepeli;
+    }
+
+}
---
+++ app/src/main/java/com/example/appdual/Class/RecyclerAdapter.java
@@ -0,0 +1,55 @@
+package com.example.appdual.Class;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.appdual.R;
+
+import java.util.ArrayList;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> {
+
+    ArrayList data = new ArrayList();
+    Context context;
+
+    public RecyclerAdapter(Context ct, String nom[]){
+        this.context = ct;
+        this.data = itemList;
+        Log.i("testNom", "-- " + data.size());
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.myText.setText((Integer) data.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView myText;
+
+        public MyViewHolder (@NonNull View itemView){
+            super(itemView);
+            myText = itemView.findViewById(R.id.Noms);
+        }
+    }
+}
