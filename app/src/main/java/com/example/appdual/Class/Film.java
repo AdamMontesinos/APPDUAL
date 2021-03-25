ef1a37a00005120da25b4bcb142c3af1dd16d736
cf19adan.montesinos <cf19adan.montesinos>
2021-03-25 16:20:22 +0100

    Adam

--- app/src/main/java/com/example/appdual/Class/Film.java
+++ app/src/main/java/com/example/appdual/Class/Film.java
@@ -2,9 +2,12 @@ package com.example.appdual.Class;
 
 public class Film {
     public String nombrepeli;
+    //public String img;
 
-    public Film(String title) {
+    public Film(String title/*,String img*/) {
         this.nombrepeli = title;
+        //this.img = img;
+
     }
 
     public String getNombrepeli() {
@@ -14,5 +17,14 @@ public class Film {
     public void setNombrepeli(String nombrepeli) {
         this.nombrepeli = nombrepeli;
     }
+
+    /*public String getImagen() {
+        return img;
+    }
+
+    public void setImagen(String img) {
+        this.img = img;
+    }
+*/
 
 }
